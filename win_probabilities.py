377a741531340d932e5f9da79684128bb1dd3392
Antti Valli <antti.valli@gmail.com>
2021-03-28 00:15:27 +0200

    Calcultate probabilities and confidence interval

--- win_probabilities.py
+++ win_probabilities.py
@@ -4,6 +4,7 @@ import bots.simple
 from tqdm import tqdm
 from tabulate import tabulate
 from bots import bot_type
+import math
 
 def create_controllers(player_names):
     return [bot_type[player_names[i]](i) for i in range(4)]
@@ -16,6 +17,8 @@ def simulate_game(player_names):
         simulator.update()
     return simulator.get_winner()
 
+def confidence_limit(p, n):
+    return 1.96 * math.sqrt(p*(1.0 - p)/n)
 
 if len(sys.argv) == 3:
     player_names = [sys.argv[1], sys.argv[1], sys.argv[2], sys.argv[2]]
@@ -23,11 +26,13 @@ else:
     print("usage: win_probabilities bot1 bot2")
     sys.exit()
 winners = [0, 0, 0]
-n_games = 100
+n_games = 200
 for i in tqdm(range(n_games)):
     result = simulate_game(player_names)
     winners[result] += 1
-
-info = {'Result':['Draw', sys.argv[1], sys.argv[2]], 'Count': winners}
+probs = [x / float(n_games) for x in winners]
+info = {'Result':['Draw', sys.argv[1], sys.argv[2]],
+        'Prob.': probs,
+        'Conf. Int.': [confidence_limit(probs[i], n_games) for i in range(3)]}
 
 print(tabulate(info, headers='keys', tablefmt = "fancy_grid"))
